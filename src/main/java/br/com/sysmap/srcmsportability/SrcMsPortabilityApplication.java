d543e4da16c9e844c4a4a7a149d62b000782db61
carlomagnorocha <carlomagnorocha@gmail.com>
2022-03-07 11:18:48 -0300

    cria estrutura inicial

---
+++ src/main/java/br/com/sysmap/srcmsportability/SrcMsPortabilityApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.srcmsportability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcMsPortabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcMsPortabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sysmap/srcmsportability/SrcMsPortabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.srcmsportability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcMsPortabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
