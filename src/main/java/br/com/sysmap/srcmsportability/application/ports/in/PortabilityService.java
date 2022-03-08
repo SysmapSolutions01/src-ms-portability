d4aaebe59e7f6cccbd9478c5c9f7721875fefad5
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-03-08 10:29:35 -0300

    Changed PortabilityService method param to InputPortability

--- src/main/java/br/com/sysmap/srcmsportability/application/ports/in/PortabilityService.java
+++ src/main/java/br/com/sysmap/srcmsportability/application/ports/in/PortabilityService.java
@@ -1,7 +1,8 @@
 package br.com.sysmap.srcmsportability.application.ports.in;
 
 import br.com.sysmap.srcmsportability.domain.Portability;
+import br.com.sysmap.srcmsportability.framework.adapters.in.dtos.InputPortability;
 
 public interface PortabilityService {
-    Portability newPortability(Portability portability);
+    Portability newPortability(InputPortability portability);
 }
--- src/main/java/br/com/sysmap/srcmsportability/application/services/PortabilityServiceImpl.java
+++ src/main/java/br/com/sysmap/srcmsportability/application/services/PortabilityServiceImpl.java
@@ -2,11 +2,12 @@ package br.com.sysmap.srcmsportability.application.services;
 
 import br.com.sysmap.srcmsportability.application.ports.in.PortabilityService;
 import br.com.sysmap.srcmsportability.domain.Portability;
+import br.com.sysmap.srcmsportability.framework.adapters.in.dtos.InputPortability;
 
 public class PortabilityServiceImpl implements PortabilityService {
 
     @Override
-    public Portability newPortability(Portability portability) {
+    public Portability newPortability(InputPortability portability) {
         return null;
     }
 }
