dd1e723799a6c4b03aaee49a3d2bfde88cdc4d05
jvfq2000 <jvfq2000@gmail.com>
2022-03-08 14:27:27 -0300

    corrige nome do dto de OutputPortability para OutputPortabilityCreated

--- src/main/java/br/com/sysmap/srcmsportability/framework/adapters/in/rest/dtos/OutputPortabilityCreated.java
+++ src/main/java/br/com/sysmap/srcmsportability/framework/adapters/in/rest/dtos/OutputPortabilityCreated.java
@@ -2,14 +2,13 @@ package br.com.sysmap.srcmsportability.framework.adapters.in.rest.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
-public class OutputPortability implements Serializable {
+public class OutputPortabilityCreated implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private UUID portabilityId;
